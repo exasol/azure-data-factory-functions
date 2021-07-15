749a080d2886ab6269432587b50aacae37132630
Pieterjan Spoelders <pj.spoelders@gmail.com>
2021-07-15 14:37:52 +0200

    removed code in comments

--- ExaFunctions/Utilities/RequestUtilities.cs
+++ ExaFunctions/Utilities/RequestUtilities.cs
@@ -22,27 +22,5 @@ namespace Exasol.Utilities
             return data;
         }
 
-        //public static string GetPropertyFromDynamic(dynamic dyn, string propertyname, string description = null)
-        //{
-        //    string property;
-        //    try
-        //    {
-        //        property = dyn.;
-        //    } catch (Exception e)
-        //    {
-        //        string errorMsg = "";
-        //        if (description== null)
-        //        {
-        //            errorMsg = $"The request body is missing {propertyname}";
-        //        } else
-        //        {
-        //            errorMsg = $"The request body is missing {propertyname}, { description }";
-                    
-        //        }
-        //        throw new Exception(errorMsg);
-        //    }
-        //    return property;
-        //}
-
     }
 }
